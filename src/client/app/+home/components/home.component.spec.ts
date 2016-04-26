055ff8b3062c848fbd0643bdef9b227b77b624b8
Rafael Negherbon <rafael.negherbon@gmail.com>
2016-04-26 19:31:27 -0300

    structure

--- src/client/app/+home/components/home.component.spec.ts
+++ src/client/app/+home/components/home.component.spec.ts
@@ -13,32 +13,7 @@ import {NameListService} from '../../shared/index';
 
 export function main() {
   describe('Home component', () => {
-    it('should work',
-      injectAsync([TestComponentBuilder], (tcb: TestComponentBuilder) => {
-        return tcb.createAsync(TestComponent)
-          .then(rootTC => {
-            rootTC.detectChanges();
 
-            let homeInstance = rootTC.debugElement.children[0].componentInstance;
-            let homeDOMEl = rootTC.debugElement.children[0].nativeElement;
-            let nameListLen = function () {
-              return homeInstance.nameListService.names.length;
-            };
-
-            expect(homeInstance.nameListService).toEqual(jasmine.any(NameListService));
-            expect(nameListLen()).toEqual(4);
-            expect(DOM.querySelectorAll(homeDOMEl, 'li').length).toEqual(nameListLen());
-
-            homeInstance.newName = 'Minko';
-            homeInstance.addName();
-            rootTC.detectChanges();
-
-            expect(nameListLen()).toEqual(5);
-            expect(DOM.querySelectorAll(homeDOMEl, 'li').length).toEqual(nameListLen());
-
-            expect(DOM.querySelectorAll(homeDOMEl, 'li')[4].textContent).toEqual('Minko');
-          });
-      }));
   });
 }
 
--- src/client/main.ts
+++ src/client/main.ts
@@ -9,14 +9,3 @@ bootstrap(AppComponent, [
   ROUTER_PROVIDERS,
   provide(APP_BASE_HREF, { useValue: '<%= APP_BASE %>' })
 ]);
-
-// In order to start the Service Worker located at "./worker.js"
-// uncomment this line. More about Service Workers here
-// https://developer.mozilla.org/en-US/docs/Web/API/Service_Worker_API/Using_Service_Workers
-//
-// if ('serviceWorker' in navigator) {
-//   (<any>navigator).serviceWorker.register('./worker.js').then((registration: any) =>
-//       console.log('ServiceWorker registration successful with scope: ', registration.scope))
-//     .catch((err: any) =>
-//       console.log('ServiceWorker registration failed: ', err));
-// }
