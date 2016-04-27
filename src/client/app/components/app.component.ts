d9171a3b28b41e60ab8c74602d6b165789dbd823
Rafael Negherbon <rafael.negherbon@gmail.com>
2016-04-27 19:28:38 -0300

    Filter to prevent restrict access to restrict routes

--- src/client/app/components/app.component.ts
+++ src/client/app/components/app.component.ts
@@ -1,21 +1,27 @@
 import {Component} from 'angular2/core';
-import {ROUTER_DIRECTIVES, RouteConfig} from 'angular2/router';
+import {RouteConfig} from 'angular2/router';
 import {NavbarComponent} from './navbar.component';
 import {ToolbarComponent} from './toolbar.component';
-import {NameListService} from '../shared/index';
 import {HomeComponent} from '../+home/index';
+import {LoginComponent} from '../+login/index';
+import {LoggedInRouterOutlet} from '../loggedin.outlet';
 
 @Component({
   selector: 'sd-app',
-  viewProviders: [NameListService],
   templateUrl: 'app/components/app.component.html',
-  directives: [ROUTER_DIRECTIVES, NavbarComponent, ToolbarComponent]
+  directives: [LoggedInRouterOutlet]
 })
 @RouteConfig([
+  { path: '/', redirectTo: ['/home'] },
   {
-    path: '/',
+    path: '/home',
     name: 'Home',
     component: HomeComponent
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: LoginComponent
   }
 ])
 export class AppComponent {}
